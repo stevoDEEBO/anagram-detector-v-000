47217a1384d577efa17db52f30e72fc068c7b70f
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-05-23 19:25:03 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+    end
+
+  def match(array)
+    array.select do |anagram|
+      @word.split("").sort == anagram.split("").sort
+    end
+  end
+end
