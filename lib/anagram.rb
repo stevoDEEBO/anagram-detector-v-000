1394c2e75d323c59574e661f98564a0fc5758133
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-05-23 19:20:20 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @no_match = []
+    @word = word
+    end
+  
+  def match(array)
+    array.detect |word| 
+        if @word.split("").sort == word.split("").sort
+            word
+        else
+            @no_match
+    end
+  end
+end
